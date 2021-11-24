7d76b6426683929e9908b948ce2e58d460540a34
NomkoEsko <Nomondoo2004@gmail.com>
2021-11-24 12:31:14 +0800

    bargl

--- datatypes/index.js
+++ datatypes/index.js
@@ -1,12 +1,23 @@
-const string = 'apple, banana, kiwi';
+const string = "apple, banana, kiwi";
 
 // 1. Өгсөн string - ийн бүх үсгийг том болго: 'APPLE, BANANA, KIWI'
 // -- Энд кодоо бичнэ үү!
 
+for (let i = 0; i < string.length; i++) {
+  let aAscii;
+  for (let j = 0; j < 32; j++) {
+    if (string[i] !== string.charCodeAt(j)) {
+      aAscii = string[i].charCodeAt(j) - 32;
+    }
+    console.log(String.fromCharCode(aAscii))
+  }
+}
+
+
 // 2. Өгсөн string - ийг дараах хэлбэртэй болго: 'Apple; Banana; Kiwi'
 // -- Энд кодоо бичнэ үү!
 
-const array = ['d', 'e', 'c', 'a', 'b'];
+const array = ["d", "e", "c", "a", "b"];
 // 3. Өгсөн array - ыг үсгийн дарааллаар эрэмблэ.
 // -- Энд кодоо бичнэ үү!
 
